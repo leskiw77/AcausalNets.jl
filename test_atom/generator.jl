b4789305f0a84f53b702aa9b13c917ffc01c7ee1
Jeremi <j.leskiw@avsystem.com>
2020-03-22 00:06:33 +0100

    check if junction tree is connected

--- test_atom/generator.jl
+++ test_atom/generator.jl
@@ -3,9 +3,32 @@ Pkg.activate(".")
 
 using AcausalNets
 
-nodes_number = 30
-edges_number = 35
+nodes_number = 35
+edges_number = 39
 an = generate_random_acasual_net(nodes_number, edges_number)
 show(an)
 
-Pkg.resolve()
+
+observations = Evidence{Matrix{Complex{Float64}}}[]
+
+an
+
+to_infer = variables(systems(an)[1])
+
+inferred_system, debug = infer_join_tree(an, to_infer, observations)
+inferred_system_naive, debug_naive = infer_naive(an, to_infer, observations)
+inferred_system_belief, debug_belief = infer_belief(an, to_infer, observations)
+
+real(distribution(inferred_system))
+real(distribution(inferred_system_naive))
+real(distribution(inferred_system_belief))
+
+(observations_jt,propagated_jt,jt,) = debug
+show(jt)
+
+jt
+
+using LightGraphs
+
+is_connected(an.dag)
+is_connected(jt.graph)
